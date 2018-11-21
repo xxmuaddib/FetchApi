11696db3874080cb85bcc5b0ce2404f48201ab26
Mikayel Saghyan <xxmuaddib@gmail.com>
2018-11-21 14:49:53 +0400

    Fix example, wrap FetchApi call in try catch

--- example.js
+++ example.js
@@ -4,13 +4,21 @@ export default class Example {
   async getSampleRequest() {
     const testProperty1 = 1;
     const testProperty2 = 2;
-    const result = await FetchApi.get(`testPath?testProperty1=${testProperty1}&testProperty2=${testProperty2}`);
-    // Do something with result.
+    try {
+      const result = await FetchApi.get(`testPath?testProperty1=${testProperty1}&testProperty2=${testProperty2}`);
+      // Do something with result.
+    } catch (e) {
+      // Do something with catch
+    }
   }
   async postSampleRequest() {
     const testProperty1 = 1;
     const testProperty2 = 2;
-    const result = await FetchApi.post('testPath', {testProperty1, testProperty2});
-    // Do something with result.
+    try {
+      const result = await FetchApi.post('testPath', {testProperty1, testProperty2});
+      // Do something with result.
+    } catch (e) {
+      // Do something with catch
+    }
   }
 }
